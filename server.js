17a578a7fbf4fc7aa3015c8169cafe54e7bcfd4c
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-03-27 21:19:21 +0530

    third commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173","https://tarunsaisrinivas.vercel.app","https://bytefolio.netlify.app/"],
     methods:["PUT","POST","GET","DELETE"],
     credentials:true,
 }));
