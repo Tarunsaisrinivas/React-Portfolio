faca008a4bee12fcff47f072f5966ac711a26dd3
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-03-28 08:41:18 +0530

    fourth

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -10,7 +10,7 @@ function Details() {
 
     const fetchMessages = async () => {
         try {
-            const response = await axios.get('https://tarunsaisrinivas.vercel.app/messages');
+            const response = await axios.get('http://localhost:5000/messages');
             setMessages(response.data);
         } catch (err) {
             console.error('Error fetching messages:', err);
