6d3ddc38e82e3b178fe8e9e7bd57ec474741fb81
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-03-28 10:09:26 +0530

    fifth

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -34,15 +34,15 @@ function Contact() {
                 <form onSubmit={handleSubmit} autoComplete='off' className="space-y-4" >
                     <div>
                         <label htmlFor="name" className="block text-sm font-medium text-white">Name:</label>
-                        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} className="block w-full p-2 mt-1 text-white bg-gray-600 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
+                        <input type="text" required id="name" name="name" value={formData.name} onChange={handleChange} className="block w-full p-2 mt-1 text-white bg-gray-600 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
                     </div>
                     <div>
                         <label htmlFor="email" className="block text-sm font-medium text-white">Email:</label>
-                        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="block w-full p-2 mt-1 text-white bg-gray-600 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
+                        <input type="email" required id="email" name="email" value={formData.email} onChange={handleChange} className="block w-full p-2 mt-1 text-white bg-gray-600 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
                     </div>
                     <div>
                         <label htmlFor="message" className="block text-sm font-medium text-white">Message:</label>
-                        <textarea id="message" name="message" value={formData.message} onChange={handleChange} className="block w-full p-2 mt-1 text-white bg-gray-600 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"></textarea>
+                        <textarea id="message" required name="message" value={formData.message} onChange={handleChange} className="block w-full p-2 mt-1 text-white bg-gray-600 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"></textarea>
                     </div>
                     <button type="submit" className="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">Submit</button>
                 </form>
