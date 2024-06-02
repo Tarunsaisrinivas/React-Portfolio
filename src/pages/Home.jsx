0c1aaa1f164dcb728abee4f0c863d0599b0b2b2f
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2024-06-02 10:57:36 +0530

    Home update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ import { Tilt } from "react-tilt";
 
 const defaultOptions = {
   reverse: false, // reverse the tilt direction
-  max: 35, // max tilt rotation (degrees)
+  max: 80, // max tilt rotation (degrees)
   perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
   scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
   speed: 1000, // Speed of the enter/exit transition
@@ -94,7 +94,7 @@ const Home = ({ darkMode }) => {
         </div>
         {/* Image */}
           <div className="mt-8 md:mt-0 md:w-1/2" data-aos="fade-left">
-        <Tilt options={defaultOptions}>
+        <Tilt options={defaultOptions} className="transition-all">
             <img
               src="/Cartoon-Avatar-On-Circle-Background-removebg-preview.png"
               alt=""
