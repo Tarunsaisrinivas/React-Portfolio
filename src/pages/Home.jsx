9cff31e33dfe7fd899dec3d5fdde55a72e44f1dc
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2024-06-02 10:53:52 +0530

    home update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,19 @@ import { FaGithub, FaLinkedin, FaFileAlt } from "react-icons/fa";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./home.css";
+import { Tilt } from "react-tilt";
+
+const defaultOptions = {
+  reverse: false, // reverse the tilt direction
+  max: 35, // max tilt rotation (degrees)
+  perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 1000, // Speed of the enter/exit transition
+  transition: true, // Set a transition on enter/exit.
+  axis: null, // What axis should be disabled. Can be X or Y.
+  reset: true, // If the tilt effect has to be reset on exit.
+  easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+};
 
 const Home = ({ darkMode }) => {
   useEffect(() => {
@@ -80,13 +93,15 @@ const Home = ({ darkMode }) => {
           </div>
         </div>
         {/* Image */}
-        <div className="mt-8 md:mt-0 md:w-1/2" data-aos="fade-left">
-          <img
-            src="/Cartoon-Avatar-On-Circle-Background-removebg-preview.png"
-            alt=""
-            className="m-auto rounded-full w-44 md:w-64 lg:w-80"
-          />
-        </div>
+          <div className="mt-8 md:mt-0 md:w-1/2" data-aos="fade-left">
+        <Tilt options={defaultOptions}>
+            <img
+              src="/Cartoon-Avatar-On-Circle-Background-removebg-preview.png"
+              alt=""
+              className="m-auto cursor-pointer rounded-full w-44 md:w-64 lg:w-80 "
+            />
+        </Tilt>
+          </div>
       </div>
 
       {/* <p className="flex justify-center items-center space-x-4">
